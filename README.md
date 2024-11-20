8966c236952ddbc50252619ed0cc64b9a818b191
lordace-coder <lordyacey@gmail.com>
2024-11-20 11:01:00 +0100

    completed

--- README.md
+++ README.md
@@ -6,6 +6,10 @@
 
 <!-- tenary operator -->
 
-    ["cook","clean"]
-
-    "[   "cook","clean"]"
+todo = [1,2,3,4,5,6,]
+x = [...todo]
+todo = 'lalal"
+<!-- dsjdvdjjd -->
+setTodo([...todo])
+<!-- memory address -->
+<!-- 0x272359620512894589127549128534691253645674146735 -->
--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,65 @@ import NoTodo from "./components/NoTodo";
 
 function App() {
   const input = useRef();
-
   const [todo, setTodo] = useState(
     JSON.parse(localStorage.getItem("todos")) ?? []
+    // []
   );
-  
+
   useEffect(() => {
-    if (todo.length > 0) {
-      localStorage.setItem("todos", JSON.stringify(todo));
-    }
+    localStorage.setItem("todos", JSON.stringify(todo));
   });
 
+  const countCompletedTodo = () => {
+    var count = 0;
+    todo.forEach((i) => {
+      if (i.completed) {
+        count++;
+      }
+    });
+    return count;
+  };
+
+  const markTodoAsCompleted = (id) => {
+    var todoIndex = todo.findIndex((item) => item.id == id);
+    const newTodoList = [...todo]; //DESTRUCTURING
+    var selectedTodo = newTodoList[todoIndex];
+    selectedTodo.completed = !selectedTodo.completed;
+    newTodoList[todoIndex] = selectedTodo;
+    setTodo(newTodoList);
+  };
+
+  const deleteTodo = (id) => {
+    var todoIndex;
+    var selectedTodo;
+    todoIndex = todo.findIndex((s) => s.id == id);
+    const newTodoList = [...todo];
+    newTodoList.pop(todoIndex);
+    setTodo(newTodoList);
+  };
+  const newTodo = () => {
+    // if todo is empty
+
+    if (
+      input.current.value == " " ||
+      input.current.value == "" ||
+      input.current.value == null
+    ) {
+      alert("Todo cant be empty");
+      return;
+    }
+
+    setTodo([
+      {
+        id: todo.length + 1,
+        todo: input.current.value,
+        completed: false,
+      },
+      ...todo,
+    ]);
+
+    input.current.value = null;
+  };
   return (
     // <PlayGround />
     <div>
@@ -33,21 +81,7 @@ function App() {
           />
 
           <button
-            onClick={() => {
-              // if todo is empty
-
-              if (
-                input.current.value == " " ||
-                input.current.value == "" ||
-                input.current.value == null
-              ) {
-                alert("Todo cant be empty");
-                return;
-              }
-
-              setTodo([input.current.value, ...todo]);
-              input.current.value = null;
-            }}
+            onClick={newTodo}
             className="bg-blue-400 text-white p-2 rounded-full
         text-sm hover:bg-blue-600 transition-all
         "
@@ -60,14 +94,20 @@ function App() {
       {/* stats */}
       <div className="flex justify-between px-6 text-white font-semibold">
         <p>Total Todos: {todo.length}</p>
-        <p>Completed Todos: {todo.length}</p>
+        <p>Completed Todos: {countCompletedTodo()}</p>
       </div>
 
       {todo.length == 0 && <NoTodo />}
 
       <div className="px-10 pt-10 flex flex-col gap-5">
         {todo.map((item) => (
-          <TodoItem todo={item} />
+          // key in jsx
+          <TodoItem
+            todo={item}
+            key={item.id}
+            onCheck={markTodoAsCompleted}
+            onDelete={deleteTodo}
+          />
         ))}
       </div>
     </div>
